5d9ceee0bc6a98d9a32697abb6774fdcbfbb12e7
nooblong <18128966990@163.com>
2019-09-11 13:27:35 +0800

    9月11日中午

---
+++ cmake-build-debug/CMakeFiles/DataAndStruct.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataAndStruct.dir/main.cpp.obj"
+  "DataAndStruct.pdb"
+  "DataAndStruct.exe"
+  "DataAndStruct.exe.manifest"
+  "libDataAndStruct.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DataAndStruct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
