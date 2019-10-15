bde89ab9672946ff358a5989feed6abf57d3ac61
龙打野 <18128966990@163.com>
2019-10-16 00:00:52 +0800

    我傻了

--- Queue/main5.cpp
+++ Queue/main5.cpp
@@ -1,21 +1,25 @@
 //
 // Created by lyl on 2019/10/15.
 //
-#include "seqQueue.h"
+#include "seqQueue1.h"
 #include <iostream>
+
 using namespace std;
 
-int main(){
-    seqQueue<int> seqQueue(5);
-    for (int i = 0; i < 30; i++) {
+int main() {
+    seqQueue<int> seqQueue(100);
+    for (int i = 0; i < 10; ++i) {
         seqQueue.enQueue(i);
-        seqQueue.traverse();
     }
-
-    cout<<seqQueue.maxSIze;
+    for (int j = 0; j < 10; ++j) {
+        cout<<seqQueue.deQueue()<<" ";
+    }
+    seqQueue.resize();
     cout<<endl;
-            for (int j = 0; j < 20; ++j) {
-            cout<<seqQueue.deQueue()<< " ";
-        }
-        cout<<endl;
+    for (int i = 0; i < 30; ++i) {
+        seqQueue.enQueue(i);
+    }
+    for (int j = 0; j < 30; ++j) {
+        cout<<seqQueue.deQueue()<<" ";
+    }
 }
